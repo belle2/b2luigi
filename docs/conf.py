62232f4121d806bddf5d9117574f45c1ba9d42e0
Giacomo De Pietro <giacomo.pietro@kit.edu>
2023-12-11 14:12:55 +0100

    Attempt to fix the Sphinx documentation build on buildbot

--- docs/conf.py
+++ docs/conf.py
@@ -13,8 +13,9 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 
 import os
+import pathlib
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, pathlib.Path(__file__).parents[1].resolve().as_posix())
 
 
 # -- Project information -----------------------------------------------------
