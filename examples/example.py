7a94ec3a1d3ca49d67bf77980ace98efaa48b4a6
Alexander Heidelbach <alexander.heidelbach@kit.edu>
2024-10-30 12:58:12 +0100

    .

---
+++ examples/example.py
@@ -0,0 +1,18 @@
+import b2luigi
+
+
+class MyTask(b2luigi.Task):
+    parameter = b2luigi.Parameter()
+    apptainer_image = "/cvmfs/belle.cern.ch/images/belle2-base-el9"
+    apptainer_mount = ["/cvmfs"]
+
+    def run(self):
+        with open(self.get_output_file_name("output.txt"), "w") as f:
+            f.write(f"{self.parameter}")
+
+    def output(self):
+        yield self.add_to_output("output.txt")
+
+
+if __name__ == "__main__":
+    b2luigi.process(MyTask(parameter=1))
