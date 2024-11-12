3c3ff7313bfa7b6d6eb3db335f781972bca5d392
Alexander Heidelbach <alexander.heidelbach@kit.edu>
2024-11-12 10:58:43 +0100

    Make apptainer commands run in ApptainerProcess

--- b2luigi/batch/processes/apptainer.py
+++ b2luigi/batch/processes/apptainer.py
@@ -1,10 +1,16 @@
+import os
 import subprocess
-import enum
 
-
+import shlex
 from b2luigi.core.settings import get_setting
 from b2luigi.batch.processes import BatchProcess, JobStatus
-from b2luigi.core.utils import create_cmd_from_task
+from b2luigi.core.utils import create_cmd_from_task, map_folder, get_log_file_dir
+
+
+def is_subdir(path, parent_dir):
+    path = os.path.abspath(path)
+    parent_dir = os.path.abspath(parent_dir)
+    return os.path.commonpath([path, parent_dir]) == parent_dir
 
 
 class ApptainerProcess(BatchProcess):
@@ -18,72 +24,81 @@ class ApptainerProcess(BatchProcess):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._job_id = None
+        self._sdtout = None
+        self._stderr = None
 
     def get_job_status(self):
-        # In a simple execution model, we can assume the job is either running or completed.
         if self._job_id is None:
             return JobStatus.aborted
 
         try:
             # Check if the job is still running
-            process = subprocess.run(
-                ["ps", "-p", str(self._job_id)], capture_output=True
-            )
+            process = subprocess.run(["ps", "-p", str(self._job_id)], capture_output=True)
             if process.returncode == 0:
-                return JobStatus.running  # Process is still running
+                return JobStatus.running
             else:
-                return JobStatus.successful  # Process has finished
+                self._write_output()
+                return JobStatus.successful
         except Exception:
+            self._write_output()
             return JobStatus.aborted
 
     def start_job(self):
         env_setup_script = get_setting("env_script", task=self.task, default="")
         command = " ".join(create_cmd_from_task(self.task))
 
-        # 4. Forth part is to create the correct execution command
-        # (a) If a valid apptainer image is provided, build an apptainer command
-
         apptainer_image = get_setting("apptainer_image", task=self.task, default="")
 
         # If the batch system is gbasf2, we cannot use apptainer
         if get_setting("batch_system", default="lsf", task=self.task) == "gbasf2":
-            raise ValueError(
-                "Invalid batch system for apptainer usage. Apptainer is not supported for gbasf2."
-            )
+            raise ValueError("Invalid batch system for apptainer usage. Apptainer is not supported for gbasf2.")
 
         exec_command = ["apptainer", "exec"]
+        additional_params = get_setting("apptainer_additional_params", default="", task=self.task)
+        exec_command += [f" {additional_params}"] if additional_params else []
+
         # Add apptainer mount points if given
         mounts = get_setting("apptainer_mounts", task=self.task, default=[])
 
         if get_setting("apptainer_mount_defaults", task=self.task, default=True):
-            mounts += [
-                get_setting("res_dir", task=self.task),
-                get_setting("log_dir", task=self.task),
+            local_mounts = [
+                map_folder(get_setting("result_dir", task=self.task, default=".", deprecated_keys=["result_path"])),
+                get_log_file_dir(task=self.task),
             ]
+            for mount in local_mounts:
+                os.makedirs(mount, exist_ok=True)
+                if not is_subdir(mount, os.getcwd()):
+                    mounts.append(mount)
+
         for mount in mounts:
             exec_command += ["--bind", mount]
-        # Other mounts
-        additional_params = get_setting("apptainer_additional_params", default="", task=self.task)
-        exec_command += [f" {additional_params}"]
+
         exec_command += [apptainer_image]
         exec_command += ["/bin/bash", "-c"]
         exec_command += [f"'source {env_setup_script} && {command}'"]
-        print(exec_command)
+
         # Start the job and capture the job ID
-        self._job_id = subprocess.Popen(exec_command).pid
+        # Do the shlex split for correct string interpretation
+        process = subprocess.Popen(shlex.split(" ".join(exec_command)), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        self._stdout, self._stderr = process.communicate()
+        self._job_id = process.pid
 
     def terminate_job(self):
         if self._job_id is not None:
             subprocess.run(["kill", str(self._job_id)], stdout=subprocess.DEVNULL)
 
+    def _write_output(self):
+        log_file_dir = get_log_file_dir(self.task)
+
+        with open(os.path.join(log_file_dir, "stdout"), "w") as f:
+            if self._stdout is not None:
+                f.write(self._stdout.decode())
+
+        with open(os.path.join(log_file_dir, "stderr"), "w") as f:
+            if self._stderr is not None:
+                f.write(self._stderr.decode())
+
     def _create_apptainer_command(self):
         # Create command setup here if needed (optional)
 
         pass
-
-
-# JobStatus Enum is kept simple; adapt as needed
-class ApptainerJobStatus(enum.IntEnum):
-    running = 1
-    completed = 2
-    aborted = 999
