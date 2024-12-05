4381498d4949a4f6b567309f7e7bea13d26c90de
Jonas Eppelt <jonas.eppelt@kit.edu>
2024-12-05 14:48:33 +0100

    fix typehints

--- b2luigi/core/task.py
+++ b2luigi/core/task.py
@@ -41,7 +41,7 @@ class Task(luigi.Task):
                       f.write(f"{average}\\n")
     """
 
-    def add_to_output(self, output_file_name: str, target_class: Class = luigi.LocalTarget, **kwargs) -> Dict[str, luigi.LocalTarget]:
+    def add_to_output(self, output_file_name: str, target_class: type[luigi.FileSystemTarget] = luigi.LocalTarget, **kwargs) -> Dict[str, luigi.LocalTarget]:
         """
         Call this in your ``output()`` function to add a target to the list of files,
         this task will output.
@@ -214,7 +214,7 @@ class Task(luigi.Task):
         output_dict: Dict[str, luigi.target.FileSystemTarget] = utils.flatten_to_dict(self.output())
         return output_dict[key]
 
-    def _get_output_file_target(self, base_filename: str, target_class: Class = luigi.LocalTarget, **kwargs: Any) -> luigi.LocalTarget:
+    def _get_output_file_target(self, base_filename: str, target_class: type[luigi.FileSystemTarget] = luigi.LocalTarget, **kwargs: Any) -> luigi.LocalTarget:
         file_name: str = create_output_file_name(self, base_filename, **kwargs)
         return target_class(file_name)
 
