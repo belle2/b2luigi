ba195fc506961f6475673895b08c9d421047edf3
Marcel Hohmann <marcel.hohmann@desy.de>
2025-03-24 13:40:23 +0100

    init the enum instances via call

--- b2luigi/batch/processes/slurm.py
+++ b2luigi/batch/processes/slurm.py
@@ -109,7 +109,7 @@ class SlurmJobStatusCache(BatchJobStatusCache):
 
     def _get_SlurmJobStatus_from_string(self, state_string: str) -> str:
         try:
-            state = SlurmJobStatus[state_string.lower()]
+            state = SlurmJobStatus(state_string)
         except KeyError:
             raise KeyError(f"The state {state_string} could not be found in the SlurmJobStatus states")
         return state
