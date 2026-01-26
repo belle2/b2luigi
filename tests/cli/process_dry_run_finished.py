8a914897e3411946f65205d24b3cf2dfae0aacd7
Alexander Heidelbach <alexander.heidelbach@kit.edu>
2026-01-26 14:16:30 +0100

    Refine dry-run test case

--- tests/cli/process_dry_run_finished.py
+++ tests/cli/process_dry_run_finished.py
@@ -1,17 +1,26 @@
 import b2luigi
 
 
+class AlwaysExistingTarget(b2luigi.Target):
+    """
+    Target that always exists
+    """
+
+    def exists(self):
+        return True
+
+
 class MyTask(b2luigi.Task):
     """
     A simple task that does nothing but can be used to test dry run.
     """
 
     def output(self):
-        return b2luigi.LocalTarget("my_task_output.txt")
+        return AlwaysExistingTarget()
 
     def dry_run(self):
         print("This is a dummy output.\n")
 
 
 if __name__ == "__main__":
-    b2luigi.process(MyTask(), dry_run=True)
+    b2luigi.process(MyTask())
