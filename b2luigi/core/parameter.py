5aac91dd66b201e048d402f371282b4e30e82962
Alexander Heidelbach <alexander.heidelbach@kit.edu>
2026-03-27 14:25:58 +0100

    Return hashed group parameters if group is more than 5 parameters

--- b2luigi/core/parameter.py
+++ b2luigi/core/parameter.py
@@ -104,7 +104,11 @@ def wrap_parameter() -> None:
 
                 def _hash_function(x) -> str:
                     try:
-                        return "_".join(str(i) for i in sorted(x))
+                        if len(x) < 5:
+                            return "grouped_" + "_".join(str(i) for i in sorted(x))
+                        else:
+                            return "grouped_" + hashlib.md5(str(x).encode()).hexdigest()
+
                     except TypeError:
                         return str(x)
 
