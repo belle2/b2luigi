8e745009ed18baaf9e4dd00fd16509aed5d69887
Alexander Heidelbach <alexander.heidelbach@kit.edu>
2026-05-26 11:39:08 +0200

    Add show output app

--- b2luigi/cli/apps/__init__.py
+++ b2luigi/cli/apps/__init__.py
@@ -1,5 +1,6 @@
 from .remove import remove_app
 from .run import run_app
 from .test import test_app
+from .show import show_app
 
-list_of_apps = [remove_app, run_app, test_app]
+list_of_apps = [remove_app, run_app, test_app, show_app]
