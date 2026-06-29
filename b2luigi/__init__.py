d4d9518dd9c1d833a12adf34a63d14b966dc8f71
Jonas Eppelt <jonas.eppelt@kit.edu>
2026-06-29 15:48:30 +0200

    check for correct python version

--- b2luigi/__init__.py
+++ b2luigi/__init__.py
@@ -12,7 +12,7 @@ __version__ = "1.2.9"
 
 # Warn users if they're using an untested Python version
 min_python_version = (3, 8)
-max_python_version = (3, 11)
+max_python_version = (3, 12)
 if (sys.version_info[:2] > max_python_version) or (sys.version_info[:2] < min_python_version):
     warnings.warn(
         f"You are using Python {sys.version_info.major}.{sys.version_info.minor}. "
